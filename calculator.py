ac0dc945568f2058347b7f7c4b3c6d3e82f6ae52
Romjan D. Hossain <romjanhossain726526@gmail.com>
2019-11-27 17:44:40 +0600

    display size

--- calculator.py
+++ calculator.py
@@ -13,42 +13,17 @@ from math import pi, e, sin, cos, tan, log, log10, ceil, degrees, radians, exp, 
 # display.resizable(width=True, height=True)
 # # display.iconname('calculator.png')
 
-# # calculator display
-# # cal_dis = Entry(display, font=("Helvetica", 14), textvariable=StringVar(
-# # ), bg='#F9E9D9', width=40, borderwidth='12px')
-# # cal_dis = Entry(display, font=("Helvetica", 14), textvariable=StringVar(
-# # ), bg='#F9E9D9')
-# # cal_dis.grid(column=0, row=0)
-# monitor = Entry(display)
-# monitor.grid(column=1,row=0)
-
-# # buttons value
-# values = ["7", "8", "9", "/", "%", "clear", "AC",
-#           "4", "5", "6", "*", "(", ")", "**",
-#           "1", "2", "3", "-", "=", ",", "0", ".", "min", "+", "sin", "asin", "cos", "acos", "tan()",
-#           "pow", "log10", "max", "abs", "floor", "pi", "e", "log", "ceil", "degrees", "radians"]
-
-# # making thoes button displayed
-# i = 0
-# row=1
-# col = 1
-# for key in values:
-# 	padx=10
-# 	pady = 10
-# 	if i==7:
-# 		row=2
-# 		col=0
-
 class Calculator:
     def __init__(self):
         display = Tk()
-        display.geometry('500x600')
+        display.geometry('460x400')
         display.config(bg='#C9EBE7')
-        display.title('king')
+        display.title('Calculator')
         display.iconbitmap('calculator.ico')
         display.resizable(width=True, height=True)
         self.string = StringVar()
-        entry = Entry(display, textvariable=self.string)
+        entry = Entry(display,font=("Helvetica", 18), textvariable=self.string,width=30,bd=30,insertwidth=4,
+        	justify='right')
         entry.grid(row=0, column=0, columnspan=6)
         entry.configure(background="white")
         entry.focus()
@@ -104,8 +79,24 @@ class Calculator:
             col = col+1
             i = i+1
         display.mainloop()
+    def clearall(self):
+        self.string.set("")
 
+    def equals(self):
+        result=""
 
+        try:
+            result=eval(self.string.get())
+            self.string.set(result)
+        except:
+            result="INVALID INPUT"
+        self.string.set(result)
+        
+    def addChar(self,char):
+        self.string.set(self.string.get()+(str(char)))
+        
+    def delete(self):
+        self.string.set(self.string.get()[0:-1])
 # display.iconname('calculator.png')
 
 
