17d9f521533830f02b9eb6e95c6923b0576f1093
Romjan D. Hossain <romjanhossain726526@gmail.com>
2019-11-28 09:25:13 +0600

    final commit

--- calculator.py
+++ calculator.py
@@ -3,15 +3,11 @@ from math import *
 
 
 class Calculator:
-        #img = PhotoImage(Image('mitch.jpg'))
     def __init__(self):
         display = Tk()
-        #C = Canvas(display, bg='blue', height=460, width=518)
 
         img = PhotoImage('mitch.jpg')
         display.geometry('470x692')
-
-        # img_l.image = img
         display.config(bg='black')
         display.title('Calculator')
         display.iconbitmap('calculator.ico')
@@ -58,8 +54,6 @@ class Calculator:
                 row = 7
                 col = 0
             if txt == '=':
-                #photo3 = PhotoImage(file="equal.png")
-                #photosize3 = photo3.subsample(10, 10)
                 btn = Button(display, height=2, width=4, padx=padx, pady=pady,
                              text=txt, command=lambda txt=txt: self.equals())
                 btn.grid(row=row, column=col, padx=1, pady=1)
@@ -146,10 +140,6 @@ class Calculator:
 
             col += 1
             i += 1
-        # img = PhotoImage('mitch.jpg')
-        # img_l = Label(display, image=img)
-        # img_l.image = img
-        # img_l.place(x=0, y=0, relwidth=1, relheight=1)
         display.mainloop()
 
     def clearall(self):
@@ -170,7 +160,6 @@ class Calculator:
 
     def delete(self):
         self.string.set(self.string.get()[0:-1])
-# display.iconname('calculator.png')
 
 
 # running the display
